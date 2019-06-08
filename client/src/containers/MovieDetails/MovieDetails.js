8de88f8b242bf739996e57152f8beebb3cbf0329
dorothee <viard.dorothee@gmail.com>
2019-06-09 00:49:16 +0200

    style: truncate overview on small device

--- client/src/containers/MovieDetails/MovieDetails.js
+++ client/src/containers/MovieDetails/MovieDetails.js
@@ -8,6 +8,7 @@ const MovieDetails = (props) => {
   const [ movie , setMovie ] = useState({});
   const baseUrl = 'http://localhost:3001';
   const posterUrl = 'https://image.tmdb.org/t/p/w500';
+  const [ mobile, setMobile ] = useState(false);
   const { id } = props.match.params;
 
   const checkDocument = async (id) => {
@@ -21,6 +22,18 @@ const MovieDetails = (props) => {
         }
       });
   };
+
+
+  const parseText = function(text, limit){
+  if (text.length > limit){
+    for (let i = limit; i > 0; i--){
+      if(text.charAt(i) === ' ' && (text.charAt(i-1) !== ','||text.charAt(i-1) !== '.'||text.charAt(i-1) !== ';')) {
+        return text.substring(0, i) + '...';
+      }
+    }
+    return text.substring(0, limit) + '...';
+  } else return text;
+};
 
   const fetchMovieDetails = async (id) => {
     return axios.get(`${baseUrl}/details/${id}`);
@@ -36,9 +49,41 @@ const MovieDetails = (props) => {
 
   useEffect(() => {
     fetchMovieDetails(id)
-      .then(res => setMovie(res.data));
+      .then(res => setMovie(res.data))
+      .then(res => {
+        if (window.innerWidth <= 450) {
+        setMobile(true);
+      }
+      });
+
   }, [id]);
 
+  if (mobile) {
+      return (
+        <div className="full">
+        {movie.title &&
+          <div className="movie_card item">
+            <div className="info_section">
+              <div className="movie_header">
+                <img className="small" src={posterUrl + movie.poster_path} alt={movie.title}/>
+                <h1>{movie.title}</h1>
+                <h4>{moment(movie.release_date).format('YYYY')}</h4>
+                <span className="minutes">{movie.runtime} min</span>
+                <p className="type">{movie.genres[0].name}</p>
+              </div>
+              <div className="movie_desc">
+                <p className="text">
+                  {parseText(movie.overview, 160)}
+                </p>
+              </div>
+              <button onClick={() => checkDocument(movie.id)}>Play</button>
+            </div>
+            <div className="blur_back poster_back" style={{backgroundImage: `url(${posterUrl + movie.poster_path})`}}></div>
+          </div>
+        }
+        </div>
+      );
+  }
   return (
     <div className="full">
     {movie.title &&
@@ -56,7 +101,6 @@ const MovieDetails = (props) => {
               {movie.overview}
             </p>
           </div>
-
           <button onClick={() => checkDocument(movie.id)}>Play</button>
         </div>
         <div className="blur_back poster_back" style={{backgroundImage: `url(${posterUrl + movie.poster_path})`}}></div>
