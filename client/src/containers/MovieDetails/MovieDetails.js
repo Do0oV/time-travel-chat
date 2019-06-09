55ed272e9ab8b8cb48115defe92548ae44bbfed9
dorothee <viard.dorothee@gmail.com>
2019-06-09 18:29:20 +0200

    style: responsive global & playBtn movieDetails

--- client/src/containers/MovieDetails/MovieDetails.js
+++ client/src/containers/MovieDetails/MovieDetails.js
@@ -8,6 +8,7 @@ const MovieDetails = (props) => {
   const [ movie , setMovie ] = useState({});
   const baseUrl = 'http://localhost:3001';
   const posterUrl = 'https://image.tmdb.org/t/p/w500';
+  const imdb_url = 'https://www.imdb.com/title/';
   const [ mobile, setMobile ] = useState(false);
   const { id } = props.match.params;
 
@@ -23,17 +24,20 @@ const MovieDetails = (props) => {
       });
   };
 
+  const sendToDetails = (id) => {
+    window.open( imdb_url + id , '_blank');
+  };
 
   const parseText = function(text, limit){
-  if (text.length > limit){
-    for (let i = limit; i > 0; i--){
-      if(text.charAt(i) === ' ' && (text.charAt(i-1) !== ','||text.charAt(i-1) !== '.'||text.charAt(i-1) !== ';')) {
-        return text.substring(0, i) + '...';
+    if (text.length > limit){
+      for (let i = limit; i > 0; i--){
+        if(text.charAt(i) === ' ' && (text.charAt(i-1) !== ','||text.charAt(i-1) !== '.'||text.charAt(i-1) !== ';')) {
+          return text.substring(0, i) + '...';
+        }
       }
-    }
     return text.substring(0, limit) + '...';
-  } else return text;
-};
+    } else return text;
+  };
 
   const fetchMovieDetails = async (id) => {
     return axios.get(`${baseUrl}/details/${id}`);
@@ -49,11 +53,13 @@ const MovieDetails = (props) => {
 
   useEffect(() => {
     fetchMovieDetails(id)
-      .then(res => setMovie(res.data))
       .then(res => {
-        if (window.innerWidth <= 450) {
-        setMobile(true);
-      }
+        setMovie(res.data);
+      })
+      .then(res => {
+        if (window.innerHeight <= 568) {
+          setMobile(true);
+        }
       });
 
   }, [id]);
@@ -61,7 +67,7 @@ const MovieDetails = (props) => {
   if (mobile) {
       return (
         <div className="full">
-        {movie.title &&
+        {movie.title && (movie.overview.length >= 160) &&
           <div className="movie_card item">
             <div className="info_section">
               <div className="movie_header">
@@ -75,8 +81,10 @@ const MovieDetails = (props) => {
                 <p className="text">
                   {parseText(movie.overview, 160)}
                 </p>
+                <button onClick={()=>sendToDetails(movie.imdb_id)} className="see">read more</button>
+                <span className="helper"></span>
               </div>
-              <button onClick={() => checkDocument(movie.id)}>Play</button>
+              <div class="btn-container"><span onClick={() => checkDocument(movie.id)} class="btn">Play</span></div>
             </div>
             <div className="blur_back poster_back" style={{backgroundImage: `url(${posterUrl + movie.poster_path})`}}></div>
           </div>
@@ -101,11 +109,12 @@ const MovieDetails = (props) => {
               {movie.overview}
             </p>
           </div>
-          <button onClick={() => checkDocument(movie.id)}>Play</button>
+          <div class="btn-container"><span onClick={() => checkDocument(movie.id)} class="btn">Play</span></div>
         </div>
         <div className="blur_back poster_back" style={{backgroundImage: `url(${posterUrl + movie.poster_path})`}}></div>
       </div>
     }
+
     </div>
   );
 };
