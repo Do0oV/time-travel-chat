5bb9dd460ad4b0e6808502d08dcdaad6b10f3001
Dorothee Viard <dviard@zinio.com>
2021-05-08 16:48:53 +0200

    update server dependencies

--- server/models/db.js
+++ server/models/db.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const { DB_USER , DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, { useNewUrlParser: true,
+useUnifiedTopology: true,
   auth: {authdb:"admin"} }, (err) => {
   if (err) return console.log(err); // eslint-disable-line no-console
   console.log('Connected to the database'); // eslint-disable-line no-console
